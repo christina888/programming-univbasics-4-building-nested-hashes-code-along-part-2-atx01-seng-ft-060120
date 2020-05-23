c200ed4f56dfd39bb740a37a2af1054df33f7705
Christina Chianelli <cechianelli@gmail.com>
2020-05-23 18:42:10 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -63,8 +63,6 @@ def add_characters
    def add_characters
     epic_tragedy[:additional_characters][{:name => "Prince Escalus"},{:name => "Apothecary"}]
 
-
-
    # After your code, the following line will return the altered hash
    epic_tragedy
 end
